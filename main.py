b707c6d48d0bd03df008eeb75f8fbf5e806e022b
Romeo Grey <romeogrey0408@gmail.com>
2025-09-28 15:09:42 -0700

    added plotting total strategy performance

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-sp500_hourly_data = yf.download("SPY", period="5y", interval="1d")
+sp500_hourly_data = yf.download("SPY", period="1y", interval="1d")
 sp500_hourly_data['Returns'] = sp500_hourly_data['Close'].pct_change() * 100
 
 def BuyandHold():
     close_Prices = sp500_hourly_data['Close']
     start_Price = close_Prices.iloc[0]
-    print(start_Price)
+    #print(start_Price)
     end_Price = []
     end_Price.append(start_Price)
     for i in range((len(sp500_hourly_data['Returns']) - 1)):
@@ -18,9 +18,16 @@ def BuyandHold():
         
     return end_Price
 
-returns = BuyandHold()
-returns = pd.Series(returns)
-return_pct = returns.pct_change()
-print(returns)
-plt.plot(returns, color = 'blue')
+returnsBH = pd.Series(BuyandHold())
+def StrategyReturns(strat_return):
+    return_pct = strat_return.pct_change()
+    total_returns = pd.Series([0])
+
+    for i in range((len(return_pct) -1)):
+        #print(return_pct_list[i])
+        total_returns.loc[i+1] = total_returns.loc[i] + (return_pct.iloc[i + 1] * 100)
+    return total_returns
+print(StrategyReturns(returnsBH))
+plt.plot(StrategyReturns(returnsBH))
+#plt.plot(returns, color = 'blue') Show BH value
 plt.show()
