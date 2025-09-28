d8bd8d1d0c84b6a4d54e6d9af48160285baa8df5
Romeo Grey <romeogrey0408@gmail.com>
2025-09-28 15:39:03 -0700

    Updated Utils File and added image for demonstration

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ sp500_hourly_data['Returns'] = sp500_hourly_data['Close'].pct_change() * 100
 returnsBH = pd.Series(strategies.BuyandHold(sp500_hourly_data))
 BH_returns = utils.StrategyReturns(returnsBH)
 print(f"{BH_returns.iloc[-1]}")
-plt.plot(utils.StrategyReturns(returnsBH))
-#plt.plot(returnsBH, color = 'blue') # Show BH value
+print(utils.ReturnPercentage(returnsBH))
+
+plt.subplot(1,3,1)
+plt.plot(utils.ReturnPercentage(returnsBH), color = 'red') # Show Daily Return Percentage
+plt.title("Daily Return Percentage")
+
+plt.subplot(1,3,2)
+plt.plot(utils.StrategyReturns(returnsBH), color = 'green') # Show Total Return Percentage
+plt.title("Total Return Percentage")
+
+plt.subplot(1,3,3)
+plt.plot(returnsBH, color = 'blue') # Show BH value
+plt.title("BH Value")
+
 plt.show()
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import pandas as pd
+def ReturnPercentage(strat_return):
+    return strat_return.pct_change() * 100
 def StrategyReturns(strat_return):
-    return_pct = strat_return.pct_change()
+    return_pct = ReturnPercentage(strat_return)
     total_returns = pd.Series([0])
 
     for i in range((len(return_pct) -1)):
         #print(return_pct_list[i])
-        total_returns.loc[i+1] = total_returns.loc[i] + (return_pct.iloc[i + 1] * 100)
+        total_returns.loc[i+1] = total_returns.loc[i] + (return_pct.iloc[i + 1])
     return total_returns
