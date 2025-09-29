442c4d8a8ca64403d0cf1516ffae19e631c9f690
Romeo Grey <romeogrey0408@gmail.com>
2025-09-28 18:19:15 -0700

    Added a basic Momentum Strategy and fixed some formating.

--- main.py
+++ main.py
@@ -7,27 +7,30 @@ import strategies
 sp500_hourly_data = yf.download("SPY", period="1y", interval="1d")
 sp500_hourly_data['Returns'] = sp500_hourly_data['Close'].pct_change() * 100
 returnsBH = pd.Series(strategies.BuyandHold(sp500_hourly_data))
-returnsMA = pd.Series(strategies.MovingAverage(sp500_hourly_data))
+returnsMA = pd.Series(strategies.MovingAverage(sp500_hourly_data, 20, 100))
+returnsMOM = pd.Series(strategies.MomentumStrategy(sp500_hourly_data, 5))
 BH_returns = utils.StrategyReturns(returnsBH)
 #print(f"{BH_returns.iloc[-1]}")
 #print(utils.ReturnPercentage(returnsBH))
 
 plt.subplot(1,3,1)
-plt.plot(utils.ReturnPercentage(returnsBH), color = 'blue', label = 'BH Strategy') # Show Daily Return Percentage for BH
-plt.plot(utils.ReturnPercentage(returnsMA), color = 'red', label = 'MA Strategy') # Show Daily Return Percentage for MA
+plt.plot(utils.ReturnPercentage(returnsBH), color = 'blue', label = 'BH Strategy') # Show Daily Return Percentage for BH Strategy
+plt.plot(utils.ReturnPercentage(returnsMA), color = 'red', label = 'MA Strategy') # Show Daily Return Percentage for MA Strategy
+plt.plot(utils.ReturnPercentage(returnsMOM), color = 'green', label = 'MOM Strategy') # Show Daily Return Percentage For MOM Strategy
 plt.legend()
 plt.title("Daily Return Percentage")
 
 plt.subplot(1,3,2)
-plt.plot(utils.StrategyReturns(returnsBH), color = 'blue', label='BH Strategy') # Show Total Return Percentage for BH
-plt.plot(utils.StrategyReturns(returnsMA), color = 'red', label = 'BH Strategy') # Show Total Return Percentage for MA
+plt.plot(utils.StrategyReturns(returnsBH), color = 'blue', label='BH Strategy') # Show Total Return Percentage for BH Stratety
+plt.plot(utils.StrategyReturns(returnsMA), color = 'red', label = 'MA Strategy') # Show Total Return Percentage for MA Strategy
+plt.plot(utils.StrategyReturns(returnsMOM), color = 'green', label = 'MOM Strategy') # Show Total Return Percentage for MOM Strategy
 plt.legend()
 plt.title("Total Return Percentage")
 
 plt.subplot(1,3,3)
-
 plt.plot(returnsBH, color = 'blue', label = 'BH Strategy') # Show BH value
 plt.plot(returnsMA, color = 'red', label = 'MA Strategy') # Show MA value
+plt.plot(returnsMOM, color = 'green', label = 'MOM Strategy') # Show MOM value
 plt.legend()
 plt.title("Strat values")
 
--- strategies.py
+++ strategies.py
@@ -25,13 +25,36 @@ def MovingAverage(sp500_data, short = 20, long = 100):
     long_ma = close_PricesMA.rolling(long).mean().dropna()
     short_ma = short_ma.iloc[-returnlength:,0]
     long_ma = long_ma.iloc[-returnlength:,0]
+
     ma_df = pd.DataFrame({'short': short_ma, 'long': long_ma}).dropna()
+
     signal = (ma_df['short'] > ma_df['long']).astype(int).tolist()
+
     portfolioValue = [close_Price.iloc[0]]
+
     for i in range(1, len(close_Price)):
         if signal[i] == 1: 
             portfolioValue.append(portfolioValue[-1] * (close_Price.iloc[i] / close_Price.iloc[i-1]))
         else:
             portfolioValue.append(portfolioValue[-1])
+
     return portfolioValue
+
+def MomentumStrategy(sp500_data, lookback = 5):
+    returnlength = len(sp500_data['Returns'].tolist())
+    sp500_hourly_data = yf.download("SPY", period=f"{returnlength + (lookback*5)}d", interval="1d")
+    close_PricesMomentum = sp500_hourly_data['Close']
+    close_Price = sp500_data['Close']
+    momentum = close_PricesMomentum.pct_change(lookback).dropna()
+    momentum = momentum.iloc[-returnlength:,0]
+
+    signal = (momentum > 0).astype(int).tolist()
     
+    portfolioValue = [close_Price.iloc[0]]
+
+    for i in range(1, len(close_Price)):
+        if signal[i] == 1:
+            portfolioValue.append(portfolioValue[-1] * (close_Price.iloc[i] / close_Price.iloc[i-1]))
+        else:
+            portfolioValue.append(portfolioValue[-1])
+    return portfolioValue
